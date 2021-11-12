531c0f3fb820bb97d5d300785b55914aab0f5d72
Jiayu Yi <yijiayu@gmail.com>
2021-11-12 15:04:45 +0800

    Pass through Authorization header

--- server.rb
+++ server.rb
@@ -13,8 +13,11 @@ end
 get '/:group/:repo/:workflow' do |group, repo, workflow|
   content_type 'application/xml'
 
-  response = Net::HTTP.get(URI("https://api.github.com/repos/#{group}/#{repo}/actions/workflows/#{workflow}/runs"))
-  payload = JSON.parse(response)
+  uri = URI("https://api.github.com/repos/#{group}/#{repo}/actions/workflows/#{workflow}/runs")
+  req = Net::HTTP::Get.new(uri)
+  req['Authorization'] = request.env['HTTP_AUTHORIZATION']
+  res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
+  payload = JSON.parse(res.body)
 
   GithubToCCTray.new.convert_to_xml(payload, params[:branch])
 end
